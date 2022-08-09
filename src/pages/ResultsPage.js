e44d5f70c9ca59cd34102d2ba001b7b2f543bf75
Troy Mathis <troy.mathis00@gmail.com>
2022-08-09 18:45:33 -0500

    useState for sending info to child component

--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import { useState, React } from 'react';
 import { API_URL } from "../api/api_connection";
 
 import '../styles/ResultsPage.css';
 
 function ResultsPage(props) {
+    const [city, setCity] = useState(null);
+    const getData = async () => {
+        const response = await fetch(DATABASE_URL);
+        const data = await response.json();
+        setCity(data);
+    }
 
     return (
         <main className='results-page'>
