d142f0f64461f1a62549238a9501a465dd9ff167
johndwatt <jdw23@protonmail.com>
2022-08-09 10:59:41 -0700

    adds routes and approutes to app

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import LandingPage from '../pages/LandingPage';
+import ResultsPage from '../pages/ResultsPage';
+
+/**
+ * Returns all defined routes for links throughout application.
+ * @param {Object} props React props object. 
+ * @returns Routes JSX for react-router-dom links.
+ */
+ function AppRoutes(props) {
+    return (
+        <Routes>
+            <Route exact path='/' element={<LandingPage />} /> 
+            <Route exact path='/results' element={<ResultsPage />} />
+        </Routes>
+    );
+}
+
+export default AppRoutes;
