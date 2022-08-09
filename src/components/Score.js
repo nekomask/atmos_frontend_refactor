a765cfd2498426b95ce04626e2de2880ba61bf7e
Dan Ventura <danice.ventura@gmail.com>
2022-08-09 16:39:30 -0600

    added Score and Recs components

---
+++ src/components/Score.js
@@ -0,0 +1,10 @@
+const Score = (props) => {
+  return (
+    <div className="AQIContainer">
+      <h2 className="AQITitle">AQI Score</h2>
+      {/* <h1 className="AQIScore">{props.score}</h1> */}
+    </div>
+  )
+}
+
+export default Score
