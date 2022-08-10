b7eb11b86a2635762a29231afe7467f49b4c62da
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-08-10 13:25:59 -0700

    Add ScoreMeter style

--- src/components/ScoreMeter.js
+++ src/components/ScoreMeter.js
@@ -5,61 +5,63 @@ export default function ScoreMeter(props)
   let { size = 150 } = props;
 
   const [currColor, setCurrColor] = useState(null);
+  const [currGradient, setCurrGradient] = useState(null);
 
   useEffect(() =>
   {
     if (props.score <= 50)
     {
-      setCurrColor("radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(146, 255, 132, 0) 26.04%, rgba(196, 241, 189, 0.545396) 66.38%, #95FF84 100%)");
+      setCurrColor("rgba(146, 255, 132, 0)");
+      setCurrGradient("radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(146, 255, 132, 0) 26.04%, rgba(196, 241, 189, 0.545396) 66.38%, #95FF84 100%)");
     }
     else if (props.score <= 100)
     {
-      setCurrColor("radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(146, 255, 132, 0) 26.04%, rgba(196, 241, 189, 0.545396) 66.38%, #95FF84 100%)");
+      setCurrColor("rgba(254, 205, 31, 0)");
+      setCurrGradient("radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(254, 205, 31, 0) 26.04%, rgba(254, 205, 31, 0.55) 66.38%, #FECD1F 100%)");
     }
     else if (props.score <= 150)
     {
-      setCurrColor("radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(146, 255, 132, 0) 26.04%, rgba(196, 241, 189, 0.545396) 66.38%, #95FF84 100%)");
+      setCurrColor("rgba(255, 154, 0, 0)");
+      setCurrGradient("radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(255, 154, 0, 0) 26.04%, rgba(255, 154, 0, 0.55) 66.38%, #FF9A00 100%)");
     }
     else if (props.score <= 200)
     {
-      setCurrColor("radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(146, 255, 132, 0) 26.04%, rgba(196, 241, 189, 0.545396) 66.38%, #95FF84 100%)");
+      setCurrColor("rgba(237, 66, 29, 0)");
+      setCurrGradient("radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(237, 66, 29, 0) 26.04%, rgba(237, 66, 29, 0.55) 66.38%, #ED421D 100%)");
     }
     else if (props.score <= 300)
     {
-      setCurrColor("radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(146, 255, 132, 0) 26.04%, rgba(196, 241, 189, 0.545396) 66.38%, #95FF84 100%)");
+      setCurrColor("rgba(135, 69, 220, 0)");
+      setCurrGradient("background: radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(135, 69, 220, 0) 26.04%, rgba(135, 69, 220, 0.5) 66.38%, #8745DC 100%)");
     }
     else if (props.score <= 500)
     {
-      setCurrColor("radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(146, 255, 132, 0) 26.04%, rgba(196, 241, 189, 0.545396) 66.38%, #95FF84 100%)");
+      setCurrColor("rgba(100, 27, 135, 0)");
+      setCurrGradient("radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(100, 27, 135, 0) 27.75%, rgba(100, 27, 135, 0.55) 57.15%, #641B87 78.7%)");
     }
   }, []);
 
-  console.log(currColor);
-
   let outerCircleStyle =
   {
-    "height": 150,
-    "width": 150,
-    borderRadius: 150,
-    "background": "radial-gradient(51.79% 51.79% at 35.38% 45.13%, rgba(146, 255, 132, 0) 26.04%, rgba(196, 241, 189, 0.545396) 66.38%, #95FF84 100%)",
+    "height": size,
+    "width": size,
+    "border-radius": size,
+    "background": currGradient,
     "display": "flex",
-    justifyContent: "center",
-    alignItems: "center"
+    "justify-content": "center",
+    "align-items": "center"
   }
 
   let innerCircleStyle =
   {
-    "height": 100,
-    "width": 100,
+    "height": size * 0.75,
+    "width": size * 0.75,
     "background": "white",
-    borderRadius: 150,
+    "border-radius": size,
     "display": "flex",
-    justifyContent: "center",
-    alignItems: "center"
+    "justify-content": "center",
+    "align-items": "center"
   }
-
-  console.log(outerCircleStyle);
-  console.log(innerCircleStyle)
 
   return (
     <div
@@ -78,54 +80,6 @@ export default function ScoreMeter(props)
           <h1>{props.score}</h1>
         </div>
       </div>
-      {/* <svg
-        className="score-meter-indicator"
-        style={{ width: size, height: size }}>
-        <radialGradient id="linear-colors" cx="0.5" cy="0.5" r="0.4" fx="0.75" fy="0.75">
-          <stop offset="0%" stop-color="rgb(0, 228, 0, 0.2)" />
-          <stop offset="100%" stop-color="rgb(0, 228, 0, .1)" />
-          
-          background: radial-gradient(
-            51.79% 51.79% at 35.38% 45.13%, 
-            rgba(149, 255, 132, 0) 26.04%, 
-            rgba(196, 241, 189, 0.545396) 66.38%, 
-            #95FF84 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected
-
-
-        </radialGradient>
-        <circle
-          className="score-meter-circle-outer"
-          cx={center}
-          cy={center}
-          fill="url(#linear-colors)"
-          r={radius}
-          stroke={""}
-          strokeWidth={trackWidth}
-          transform={`rotate(180 ${center} ${center})`} />
-        <circle
-          className="score-meter-circle-inner"
-          cx={center}
-          cy={center}
-          fill="white"
-          r={radius * 0.75}
-          stroke={""}
-          strokeWidth={indicatorWidth}
-          transform={`rotate(0 ${center} ${center})`} />
-      </svg> */}
-      {/* <div
-        className="score-meter-arrow-icon-container"
-        style={{
-          position: "absolute",
-          top: `${iconTopPosition}px`,
-          left: `${iconLeftPosition}px`
-        }}>
-        <img
-          src="https://i.imgur.com/WcL0eiw.png" alt="score-meter-arrow-icon" className="score-meter-arrow-icon"
-          style={{
-            transformOrigin: "70% 70%",
-            transform: `rotate(${iconFinalAngle}deg)`,
-          }} />
-      </div> */}
     </div>
   );
 }
