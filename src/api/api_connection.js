aaf4fc07da1d98fa96606fff76812f150e233ec1
kittenmasque <volaju@gmail.com>
2022-11-05 11:55:00 -0700

    api_connection BASE_URL changed to deployed render backend

--- src/api/api_connection.js
+++ src/api/api_connection.js
@@ -1,5 +1,5 @@
 // API location URL - ONLY UPDATE THIS ONE
-const BASE_URL = "https://atmos-backend.fly.dev"; 
+const BASE_URL = "https://atmos-eywc.onrender.com"; 
 
 // API data URL for requests
 const API_URL = BASE_URL + "/cache";
