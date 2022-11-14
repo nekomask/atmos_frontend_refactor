b904200f7c438278223be6f57a695f3782f2fcb7
kittenmasque <volaju@gmail.com>
2022-11-13 20:32:40 -0800

    changed landing page input to select menu for USStates

---
+++ src/components/States.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+let states = [{name: 'Alabama', cities: ["Birmingham", "Cuba", "Decatur", "Empire", "Huntsville", "Irondale", "Ladonia", "Leeds", "Madison", "McCalla", "Mobile", "Montgomery", "Tillmans Corner", "Tuscaloosa", "Uniontown"]}, 
+{name: 'Alaska', cities: ["Anchor Point", "Anchorage", "Barrow", "College", "Eagle River", "Ester", "Fairbanks", "Farmers Loop", "Haines", "Healy", "Homer", "Juneau", "Ketchikan", "Knik-Fairview", "Nome", "North Pole", "Palmer", "Prudhoe Bay", "Seward", "Tok", "Valdez"]}, 
+{name: 'Arizona', cities: ["Ajo", "Apache Junction", "Buckeye", "Bullhead City", "Camp Verde", "Casa Grande", "Casas Adobes", "Catalina Foothills"]}, 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 
+'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 
+'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+ 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+
+ export default states
